bf16f4e5d13b4a724c6b2c4700567f2020ebb62f
daniela1304 <daniela.todescu@yahoo.com>
2019-08-12 07:48:38 +0300

    Project dependecies

---
+++ DotNetTrainig/Program.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetTrainig
+{
+    public class Program
+    {
+        
+     
+        static void Main()
+        {
+            IWebDriver driver;
+
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            
+           Thread.Sleep(3000);
+
+            driver.Quit();
+
+        }
+
+    }
+}
